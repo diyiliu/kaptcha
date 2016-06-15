76c3bcbe514752348a435e66cc090f3ae83871a0
DIYILIU <572772828@qq.com>
2016-06-15 11:03:15 +0800

    Initial commit

---
+++ src/main/java/com/diyiliu/controller/KaptchaController.java
@@ -0,0 +1,86 @@
+package com.diyiliu.controller;
+
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.Producer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Description: KaptchaController
+ * Author: DIYILIU
+ * Update: 2016-06-15 08:58
+ */
+
+@Controller
+@RequestMapping("/kaptcha")
+public class KaptchaController {
+
+    @Resource
+    private Producer kaptchaProducer;
+
+    @RequestMapping("/image")
+    public void image(HttpServletRequest request, HttpServletResponse response) {
+
+        HttpSession session = request.getSession();
+
+        response.setDateHeader("Expires", 0);
+        // Set standard HTTP/1.1 no-cache headers.
+        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+        // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
+        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        // Set standard HTTP/1.0 no-cache header.
+        response.setHeader("Pragma", "no-cache");
+        // return a jpeg
+        response.setContentType("image/jpeg");
+
+        // create the text for the image
+        String capText = kaptchaProducer.createText();
+
+        // store the text in the session
+        session.setAttribute(Constants.KAPTCHA_SESSION_KEY, capText);
+
+        ServletOutputStream out = null;
+        try {
+            // create the image with the text
+            BufferedImage bi = kaptchaProducer.createImage(capText);
+            out = response.getOutputStream();
+            // write the data out
+            ImageIO.write(bi, "jpg", out);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @RequestMapping("/check")
+    @ResponseBody
+    public boolean check(@RequestParam("code")String code, HttpSession session){
+        String text = (String)session.getAttribute(Constants.KAPTCHA_SESSION_KEY);
+
+        if(code.equals(text)){
+
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,70 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:set var="ctx" value="${pageContext.request.contextPath}"/>
+<html>
+<head>
+    <title>验证码</title>
+</head>
+<body>
+<div> 用户登录</div>
+<div>
+    <form id="loginForm" action="${pageContext.request.contextPath}/login" method="post">
+        <div style="height:40px;">
+            <label class="tip">登&nbsp;录&nbsp;名:&nbsp;&nbsp; </label>
+            <input name="name" type="text" id="name"/>
+        </div>
+        <div style="height:40px;">
+            <label class="tip">密 &nbsp;&nbsp;码：&nbsp;&nbsp;</label>
+            <input type="password" id="password" name="password"/>
+        </div>
+        <div style="height:60px;">
+            <label class="tip">验&nbsp;证&nbsp;码:&nbsp;&nbsp; </label>
+            <input type="text" name="verifyCode" id="verifyCode" onblur="checkCode()"/>
+            <img src="${ctx}/kaptcha/image" width="110" height="26" id="kaptchaImage"
+                 onclick="changeImage()"  style="margin-bottom:-10px;"/>
+        </div>
+        <div style="height:40px;">
+            <label class="tip">结 &nbsp;&nbsp;果：&nbsp;&nbsp;</label>
+            <input type="text"  id="result" readonly="readonly" style="color: brown"/>
+        </div>
+        <div style="margin-left:15px">
+            <input type="submit" class="login-btn" value="登录"/>
+            <input type="reset" class="login-btn" style="margin-left:10px;" value="重置"/>
+        </div>
+    </form>
+</div>
+<script type="text/javascript" src="${ctx}/js/jquery-3.0.0.js"></script>
+<script type="text/javascript">
+    function changeImage() {
+        $("#kaptchaImage").attr("src", "${ctx}/kaptcha/image?nocache=" + new Date().getTime());
+    }
+
+    function checkCode() {
+        var code = $("#verifyCode").val();
+        if (code.replace(/\s/g, "") == "") {
+            $("#result").val("请输入验证码");
+        } else {
+            //异步检查验证码是否输入正确
+            var url = "${ctx}/kaptcha/check";
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: {"code": code},
+                success: function (data) {
+                    if (data == true) {
+                        //验证码正确，进行提交操作
+                        $("#result").val("输入正确 ！");
+                    } else {
+                        $("#result").val("验证码错误，请重新输入！");
+                    }
+                },
+                error: function (e) {
+                    alert(e);
+                }
+            });
+        }
+    }
+
+</script>
+</body>
+</html>
